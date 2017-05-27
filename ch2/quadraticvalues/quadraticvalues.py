9b0de36726ca7b65b3745a851837b37956e1100b
gurgalex <gurgalex@iastate.edu>
2017-05-27 07:25:20 -0500

    Add user input and plot result
    Main program gets user input
    Add function for parsing user input (limited to spaces)
    Display graph with pyplot

--- ch2/quadraticvalues/quadraticvalues.py
+++ ch2/quadraticvalues/quadraticvalues.py
@@ -1,3 +1,6 @@
+"""Quadratic equation graph plotter"""
+from matplotlib import pyplot as plt
+
 
 def calc(x_list):
     """Returns a list of values given a quadratic formula and a x-value list"""
@@ -5,3 +8,24 @@ def calc(x_list):
     y_list = [x**2 + 2*x + 1 for x in x_list]
 
     return y_list
+
+
+def parse_input(userstring):
+    """Takes supplied string and converts to a list of values"""
+    xsplit = userstring.split()
+    stringtovalues = [float(x) for x in xsplit]
+
+    return stringtovalues
+
+
+if __name__ == "__main__":
+    # get x values
+    x_input = input("Space separated x values: ")
+
+    # parse x values
+    x_values = parse_input(x_input)
+    y_values = calc(x_values)
+
+    # plot graph
+    plt.plot(x_values, y_values)
+    plt.show()
